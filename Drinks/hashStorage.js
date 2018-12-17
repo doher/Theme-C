0c3528b73134a0575d6538feb22ead9ae6b2db26
doher <alexandr.pletezhov@gmail.com>
2018-12-17 19:14:57 +0300

    added buttons and functions to them

--- Drinks/hashStorage.js
+++ Drinks/hashStorage.js
@@ -3,7 +3,7 @@
 class HashStorage {
 
     addValue(key, value) {
-        this[key] = value;
+        this[key] = {value};
     }
 
     getValue(key) {
@@ -17,8 +17,9 @@ class HashStorage {
     getKeys() {
         let arrayDrinks = [],
             i = 0;
+
         for (let key in this) {
-            arrayDrinks[i] =  key;
+            arrayDrinks[i] = key;
             i++;
         }
 
@@ -28,15 +29,25 @@ class HashStorage {
 
 let drinkStorage = new HashStorage();
 
-drinkStorage.addValue('key1', 'value1');
-drinkStorage.addValue('key2', 'value2');
-drinkStorage.addValue('key3', 'value3');
-drinkStorage.addValue('key4', 'value4');
-drinkStorage.addValue('key5', 'value5');
+function addDrink() {
+    let nameDrink = prompt('Название напитка'),
+        isAlcoholic = confirm('Алкогольный он или нет'),
+        recipeDrink = prompt('Рецепт его приготовления');
 
-console.log(drinkStorage);
-console.log(drinkStorage.getValue('key1'));
+    drinkStorage.addValue(nameDrink, recipeDrink);
+    drinkStorage.addValue('alcoholic', isAlcoholic);
+}
 
-drinkStorage.deleteValue('key2');
+function getDrink() {
+    let nameDrink = prompt('Название напитка');
 
-console.log(drinkStorage.getKeys());
+    console.log(drinkStorage.getValue(nameDrink));
+}
+
+function removeDrink() {
+
+}
+
+function getListDrink() {
+
+}
