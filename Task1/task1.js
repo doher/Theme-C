82ee5b64a6859afc46126f4832744bd6ae3b93dd
doher <alexandr.pletezhov@gmail.com>
2018-12-25 19:37:48 +0300

    added solution to Task 1

---
+++ Task1/task1.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function trasformArray(array) {
+    let zeroArray = [],
+        restArray = [],
+        lengthArray = array.length;
+
+    for (let i = 0; i < lengthArray; ++i) {
+
+        if (array[i] === 0) {
+            zeroArray.push(array[i]);
+        } else {
+            restArray.push(array[i]);
+        }        
+    }
+
+    return zeroArray.concat(restArray);
+}
+
+let array = [1, 3, false, 0, 'hjh', 2, 1, 0, 1, 23, 5, 0, 0, '13', true];
+
+console.log(trasformArray(array));
